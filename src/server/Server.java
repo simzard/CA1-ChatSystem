4d62d3ada02a3287063574a7fcc6ea68bffd0184
Simon Steinaa <me@simsteinaa.dk>
2015-09-08 11:21:56 +0200

    Initial empty project

---
+++ src/server/Server.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import interfaces.ServerInterface;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Simon, Afrooz and Ib
+ */
+public class Server implements ServerInterface {
+
+    List<ClientHandler> clients = new ArrayList();
+    
+    private void removeClient(Socket s) {
+        
+    }
+    
+    private void addClient(ClientHandler ch) {
+        
+    }
+    
+    
+    
+    @Override
+    public void stopServer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private boolean syntaxIsOK(String message) {
+        return false;
+    }
+    
+    private String processInput (String message) {
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
